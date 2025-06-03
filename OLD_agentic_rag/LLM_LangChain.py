9c330609f18f0a6acbb17a4c8477a1747e048dfb
anjali52deb <anjali52debnath@gmail.com>
2025-06-03 20:02:23 +0530

    commit

--- OLD_agentic_rag/LLM_LangChain.py
+++
@@ -1,8 +0,0 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-def split_text_into_chunks(text, chunk_size=500, chunk_overlap=50):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap
-    )
-    docs = splitter.create_documents([text])
-    return [doc.page_content for doc in docs]
--- OLD_agentic_rag/db_config.py
+++
@@ -1,2 +0,0 @@
-# db_config.py
-default_fallback_db = ("agentic_rag", "Misc_DB")
